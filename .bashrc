79a2dd47be7d5398179031d19d745106e320339d
Charles <charles@qianshengqian.com>
2016-05-20 02:49:48 +0800

    bash install script

--- .bashrc
+++ .bashrc
@@ -1,7 +1,7 @@
 ### global variables
 export ENV_BASE_PATH=~/env
-source $ENV_BASE_PATH/.alias
 source $ENV_BASE_PATH/.global_export
+source $ENV_BASE_PATH/.alias
 
 if [[ -f ~/.alias ]]; then
     source ~/.alias
--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,15 @@
+my_dir=$(cd $(dirname $0) && pwd)
+shell_type=$1
+
 install_bash() {
+    $my_dir/../bash-it/install.sh
     if [ -e ~/.bashrc ]; then
         mv ~/.bashrc ~/.bashrc_backup
     fi
     echo 'source ~/env/.bashrc' > ~/.bashrc
     source ~/.bashrc
+    install_vim
 }
-
-install_bash
 
 install_vim() {
     if [ -e ~/.vimrc ]; then
@@ -15,4 +18,7 @@ install_vim() {
     ln -s ~/env/.vimrc ~/.vimrc
 }
 
-install_vim
+cmd=install_$shell_type
+$cmd
+
+
