8e6a7173da9a4bb97d599e429970d911e32b58d3
Charles <charles@qianshengqian.com>
2016-05-20 03:03:35 +0800

    update install shell script

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,25 @@
 my_dir=$(cd $(dirname $0) && pwd)
 shell_type=$1
 
+if [ -z $shell_type ]; then
+    echo Usage: $0 "<shell_name>"
+    echo "<shell_name>" could be zsh or bash
+    exit
+fi 
+
+source $my_dir/../.global_export
+
 install_bash() {
     $my_dir/../bash-it/install.sh
-    if [ -e ~/.bashrc ]; then
-        mv ~/.bashrc ~/.bashrc_backup
+    if [[ $platform == "Darwin" ]]; then
+        if [ -e ~/.bashrc ]; then
+            mv ~/.bashrc ~/.bashrc_backup
+        fi
+        echo 'source ~/env/.bashrc' > ~/.bashrc
+        source ~/.bashrc
+    else
+        echo 'source ~/env/.bashrc' >> ~/.bashrc
     fi
-    echo 'source ~/env/.bashrc' > ~/.bashrc
-    source ~/.bashrc
     install_vim
 }
 
